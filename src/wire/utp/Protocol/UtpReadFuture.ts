09d1b860101b84cc2b977a3e841242510da18d85
ScottyPoi <kyzambo@gmail.com>
2021-12-02 17:14:13 -0700

    readFuture

--- src/wire/utp/Protocol/UtpReadFuture.ts
+++ src/wire/utp/Protocol/UtpReadFuture.ts
@@ -1,5 +1,22 @@
+import { UtpReadListener } from "./UtpReadListener";
+
+
 export class UtpReadFuture {
-    constructor() {
-        
+    buffer: Buffer | null
+    listener: UtpReadListener | null;
+    
+    constructor(buffer: Buffer) {
+        this.buffer = null 
+        this.listener = null
+    }
+
+    setListener(listener: UtpReadListener) {
+        this.listener = listener
+    }
+
+    getBytesRead() {
+        if (this.buffer != null) {
+            return this.buffer.length
+        }
     }
 }
--- src/wire/utp/Protocol/UtpWriteFuture.ts
+++ src/wire/utp/Protocol/UtpWriteFuture.ts
@@ -1,15 +1,5 @@
-export class UtpWriteFuture {
-    buffer: Buffer | null
-    listener: UtpReadListener;
-    
-    constructor(buffer: Buffer) {
-        this.buffer = null 
-    }
 
-    getBytesRead() {
-        if (this.buffer != null) {
-            return this.buffer.position
-        }
-    }
+export class UtpWriteFuture {
+    
 
 }
